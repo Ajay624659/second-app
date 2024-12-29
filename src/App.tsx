dfa2c71925c0a171db523efe68e4934fbdff9700
Ajay624659 <79611871+Ajay624659@users.noreply.github.com>
2024-12-29 13:39:08 +0530

    saroha

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [user, setUser] = useState<String>("Rajesh");
+  const [user, setUser] = useState<String>("Rajesh Saroha");
   return (
     <div className="App">
       <h2>Hi {user}</h2>
